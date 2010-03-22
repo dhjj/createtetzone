8a527703561ded183c290c1015f2d43da5f9bf1e
Ben Medina <b.medina.orton@tecplot.com>
2010-03-22 11:17:36 -0700

    Added missing headers from the Tecplot distribution.

---
+++ src/missing-tecplot-headers/toolbox/ToolboxFwd.h
@@ -0,0 +1,24 @@
+#ifndef TECPLOT_TOOLBOX_TOOLBOXFWD_H
+#define TECPLOT_TOOLBOX_TOOLBOXFWD_H
+
+namespace tecplot
+{
+namespace toolbox
+{
+class ArgList;
+class AuxData;
+class Exception;
+class InvalidArgumentException;
+class OutOfMemoryException;
+class Set;
+class StateChangeEventInterface;
+class StateChangeListenerInterface;
+class StateChangeNotifier;
+class StateChangeNotifierInterface;
+class StringList;
+class StrUtil;
+class StyleValue;
+}
+}
+
+#endif //TECPLOT_TOOLBOX_TOOLBOXFWD_H

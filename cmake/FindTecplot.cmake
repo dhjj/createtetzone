cae6b6e6a9f0ae2d7e000d5b98db6e1509469b77
Ben Medina <b.medina.orton@tecplot.com>
2010-03-29 12:29:11 -0700

    Detect if the TECPLOT_INSTALL_DIR CMake cache variable has been changed, and if so, force a re-execute of the search for Tecplot's libraries and include directories.

--- cmake/FindTecplot.cmake
+++ cmake/FindTecplot.cmake
@@ -1,10 +1,30 @@
-set (TECPLOT_INSTALL_DIR TECPLOT_INSTALL_DIR-NOTFOUND CACHE PATH "Tecplot installation directory")
+set (TECPLOT_INSTALL_DIR $ENV{TEC_360_2009} CACHE PATH "Tecplot installation directory")
 
 if (NOT EXISTS ${TECPLOT_INSTALL_DIR})
     message (FATAL_ERROR "TECPLOT_INSTALL_DIR does not exist.")
 endif ()
 
-find_path (TECPLOT_INCLUDE_DIR "TECADDON.h"
+# Detect if user changed Tecplot installation directories. If so, we need to
+# re-execute the searches for libraries and paths by re-setting the cache
+# variables.
+if (TECPLOT_INSTALL_DIR_LAST)
+    string (COMPARE NOTEQUAL "${TECPLOT_INSTALL_DIR_LAST}" "${TECPLOT_INSTALL_DIR}" TECPLOT_INSTALL_DIR_CHANGED)
+endif ()
+
+if (TECPLOT_INSTALL_DIR_CHANGED)
+    # Reset all cache variables so that find_path and find_library
+    # will re-execute the search
+    message (STATUS "Tecplot installation directory changed from ${TECPLOT_INSTALL_DIR_LAST} to ${TECPLOT_INSTALL_DIR}.")
+    set (TECPLOT_INCLUDE_DIR TECPLOT_INCLUDE_DIR-NOTFOUND)
+    set (TECPLOT_LIBTEC_LIB TECPLOT_LIBTEC_LIB-NOTFOUND)
+    set (TECPLOT_TOOLBOX_LIB TECPLOT_TOOLBOX_LIB-NOTFOUND)
+    set (TECPLOT_LIBTEC_LIB_DEBUG TECPLOT_LIBTEC_LIB_DEBUG-NOTFOUND)
+    set (TECPLOT_TOOLBOX_LIB_DEBUG TECPLOT_TOOLBOX_LIB_DEBUG-NOTFOUND)
+endif ()
+set (TECPLOT_INSTALL_DIR_LAST "${TECPLOT_INSTALL_DIR}" CACHE INTERNAL "" FORCE)
+
+find_path (TECPLOT_INCLUDE_DIR
+           "TECADDON.h"
            HINTS "${TECPLOT_INSTALL_DIR}"
            PATH_SUFFIXES include
            NO_DEFAULT_PATH)
